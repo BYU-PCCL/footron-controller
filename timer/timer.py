99559d6d7dbf9874d4f9ab2ad05fa57fb0ee4742
chris-luangrath <thechrisluangrath@gmail.com>
2021-05-19 19:09:45 -0600

    planning out timer

--- timer/timer.py
+++ timer/timer.py
@@ -1,1 +1,8 @@
-yes
+# read in json
+# shuffle list
+# do forever
+    # while list isn't empty 
+        # send id 
+        # wait for amount of time
+        # follow list
+    # reshuffle after list empty 
