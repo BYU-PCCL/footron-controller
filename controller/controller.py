8c3536bf191495685548a566963d5e91e464ef8e
Matt <powley.matthew@gmail.com>
2021-05-20 13:42:14 -0600

    made basic flask server

--- controller/controller.py
+++ controller/controller.py
@@ -1,1 +1,10 @@
-yes
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>CS TV Controller</h1><p>This is the site for the CS TV API</p>"
+
+app.run()
