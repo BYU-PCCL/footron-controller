d41e50847b85279c4c34d628fbb7434443754ceb
chris-luangrath <thechrisluangrath@gmail.com>
2021-05-21 19:39:21 -0600

    prints to console, waits for lifetime

--- timer/timer.py
+++ timer/timer.py
@@ -6,12 +6,15 @@ app_list = {
     "app_list": [
         {
             "name" : "life",
+            "lifetime" : 60,
         },
         {
             "name" : "smoke",
+            "lifetime" : 59,
         },
         {
             "name" : "hair",
+            "lifetime" : 70,
         },
     ]
 
@@ -23,13 +26,13 @@ app_list = {
 def shuffle_list(ls):
     playlist = []
     for x in ls :
-        playlist.append[""]
+        playlist.append[{}]
 
     list_length = ls.count()
     for list_length in ls :
         addAt = random.randint(0, list_length-1)
         while true:
-            if playlist[addAt] == "":
+            if playlist[addAt] == {}:
                 playlist[addAt] = x
                 break
             else:
@@ -48,8 +51,12 @@ cont = true
 
 while cont:
     playlist = shuffle_list(app_list["app_list"])
-    while playlist.count > 0:
-        
+    for app in playlist:
+        print(app[0]["name"])
+        # wait for confirmation that it's running?
+        time.sleep(app[0]["lifetime"])
+    # check for new app_list data? should it be able to be updated on the fly?
+
 # do forever
     # while list isn't empty 
         # send id 
