87aa3ecdf3f4abd2eaecb001f792733189621522
chris-luangrath <thechrisluangrath@gmail.com>
2021-05-24 14:53:21 -0600

    doesn't use time.sleep for lifetime

--- timer/timer.py
+++ timer/timer.py
@@ -1,20 +1,23 @@
+import datetime
 import random
 import time
+
+# from apscheduler.schedulers.background import BackgroundScheduler
 
 # Also gotta see what apps are actually able to be used (cameras online?)
 app_list = {
     "app_list": [
         {
             "name" : "life",
-            "lifetime" : 5,
+            "lifetime" : 30,
         },
         {
             "name" : "smoke",
-            "lifetime" : 5,
+            "lifetime" : 45,
         },
         {
             "name" : "hair",
-            "lifetime" : 5,
+            "lifetime" : 27,
         },
     ]
 
@@ -50,6 +53,9 @@ def shuffle_list(ls):
     return playlist
 
 
+# set up scheduler
+# scheduler = BackgroundScheduler()
+# scheduler.start()
 
 
 # send commands to controller
@@ -59,10 +65,19 @@ cont = True
 while cont:
     playlist = shuffle_list(app_list["app_list"])
     for app in playlist:
+        
+        
+        # scheduler.add_job(lambda : scheduler.print_jobs(), 'interval', minutes=app["lifetime"])
+        
+        # scheduler.add_job(lambda : print(app["name"]), 'interval', minutes=app["lifetime"])
         print(app["name"])
-        # print("playing: " + app["name"])
+
         # wait for confirmation that it's running?
-        time.sleep(app["lifetime"])
+        start_time = datetime.datetime.now()
+        while ((datetime.datetime.now() - start_time).total_seconds() < app["lifetime"]):
+            time.sleep(10)
+            print("10 SECONDS HAVE PASSED")
+
     # check for new app_list data? should it be able to be updated on the fly?
 
 # do forever
