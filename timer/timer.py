267c0a5ddb712a61321b7eef8d255c0d9226f6a1
chris-luangrath <thechrisluangrath@gmail.com>
2021-05-24 17:17:40 -0600

    idk

--- timer/timer.py
+++ timer/timer.py
@@ -18,13 +18,8 @@ cont = True
 while cont:
     playlist = applist
     random.shuffle(playlist)
-    # print(playlist)
-    for app in playlist:
-        
-        
-        # scheduler.add_job(lambda : scheduler.print_jobs(), 'interval', minutes=app["lifetime"])
-        # scheduler.add_job(lambda : print(app["id"]), 'interval', minutes=app["lifetime"])
-        
+    
+    for app in playlist:    
         lifetime = 9
         if 'lifetime' in app:
             lifetime = app['lifetime']
@@ -36,7 +31,6 @@ while cont:
         start_time = datetime.datetime.now()
         while ((datetime.datetime.now() - start_time).total_seconds() < lifetime):
             time.sleep(10)
-            # print("10 SECONDS HAVE PASSED")
 
     # check for new applist data? should it be able to be updated on the fly?
 
