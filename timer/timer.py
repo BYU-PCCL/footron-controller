fe53be2d76583a936591b756ff428a9c87eb4316
chris-luangrath <thechrisluangrath@gmail.com>
2021-05-26 15:51:06 -0600

    it *should* work with video endtime, gotta check tho

--- timer/timer.py
+++ timer/timer.py
@@ -21,20 +21,27 @@ while cont:
     
     for appbase in playlist:    
         app = appbase[1]
-        print(app)
-        lifetime = 9
-        if 'lifetime' in app:
-            lifetime = app['lifetime']
-        else:
-            print("lifetime not in app :(") # though there should always be a default value for lifetime in apps.py
-
+        
         r = requests.put("http://localhost:5000/current-app", headers={'Content-Type': 'application/json'}, json={'id': app['id'] })
-        print(r)
+        # print(r)
+
+        current_app = list(requests.get("http://127.0.0.1:5000/current-app").json().items())
+
+        start_time = datetime.datetime.now()
+        if 'endtime' in current_app:
+            #use endtime
+            while (datetime.datetime.now() < current_app['endtime']):
+                time.sleep(2)
+                current_app = list(requests.get("http://127.0.0.1:5000/current-app").json().items())
+        else:
+            # use lifetime
+            while ((datetime.datetime.now() - start_time).total_seconds() < app['lifetime']):
+                time.sleep(10)
+
+        
 
         # wait for confirmation that it's running?
-        start_time = datetime.datetime.now()
-        while ((datetime.datetime.now() - start_time).total_seconds() < lifetime):
-            time.sleep(10)
+        
 
     # check for new applist data? should it be able to be updated on the fly?
 
