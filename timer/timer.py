2637cb7c827d591443d6f4bebe5b1c132729d63d
chris-luangrath <thechrisluangrath@gmail.com>
2021-06-16 14:07:52 -0600

    countdown is now functional

--- timer/timer.py
+++ timer/timer.py
@@ -1,8 +1,19 @@
 import copy
 import datetime
 import random
+import os
 import requests
+import requests_unixsocket
 import time
+import urllib.parse
+
+_PLACARD_SOCKETS_PATH = os.path.join(os.environ["XDG_RUNTIME_DIR"], "placard", "socket")
+_domain_sockets_session = requests_unixsocket.Session()
+_placard_escaped_url = (
+            f"http+unix://{urllib.parse.quote_plus(_PLACARD_SOCKETS_PATH)}"
+        )
+
+
 
 # from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -35,10 +46,28 @@ cont = True
 def should_advance(start_time, app):
         current_app = requests.get("http://127.0.0.1:5000/current-app").json()
         if 'end_time' in current_app:
+            _domain_sockets_session.post(
+                f"{_placard_escaped_url}/end-time",
+                json={'end_time': current_app['end_time']},
+            )
+            print(current_app['end_time'])
+
             if datetime.datetime.now() < datetime.datetime.fromtimestamp(current_app['end_time']):
                 return False
         else:
-            if ((datetime.datetime.now() - start_time).total_seconds() < app['lifetime']):
+            end_time = start_time + (app['lifetime'])
+            print("start_time")
+            print(start_time)
+            print("life")
+            print(app['lifetime'])
+            print("end")
+            print(end_time)
+            current_date = datetime.datetime.now().timestamp()
+            _domain_sockets_session.post(
+                f"{_placard_escaped_url}/end-time",
+                json={'end_time': end_time},
+            )
+            if ((current_date - start_time) < app['lifetime']):
                 return False
 
         return True
@@ -62,13 +91,14 @@ while cont:
         # print(r)
 
         current_app = list(requests.get("http://127.0.0.1:5000/current-app").json().items())
-        start_time = datetime.datetime.now()
+        # start_time = (datetime.datetime.now() - datetime.datetime(1970,1,1)).total_seconds()
+        start_time = datetime.datetime.now().timestamp()
         advance = False
 
         # wait for confirmation that it's running?
 
         while(not advance):
-            time.sleep(5)
+            time.sleep(1)
             advance = should_advance(start_time, app)
         
 
