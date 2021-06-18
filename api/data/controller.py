d4d872d81ea0c1bea3b4c1090939ce1cd3958712
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-17 18:52:05 -0600

    Pull endpoint names in controller_api out into constants

--- api/data/controller.py
+++ api/data/controller.py
@@ -5,6 +5,10 @@ from .. import protocol
 
 CONTROLLER_URL = "http://localhost:5000"
 
+_ENDPOINT_EXPERIENCES = "/experiences"
+_ENDPOINT_COLLECTIONS = "/collections"
+_ENDPOINT_CURRENT_EXPERIENCE = "/current"
+
 
 class ControllerApi:
     def __init__(self, aiohttp_session=aiohttp.ClientSession()):
@@ -25,24 +29,26 @@ class ControllerApi:
 
     async def experiences(self, use_cache=True) -> JsonDict:
         if self._experiences is None or not use_cache:
-            self._experiences = await self._get_json_response("/experiences")
+            self._experiences = await self._get_json_response(_ENDPOINT_EXPERIENCES)
 
         return self._experiences
 
     async def collections(self, use_cache=True) -> JsonDict:
         if self._collections is None or not use_cache:
-            self._collections = await self._get_json_response("/collections")
+            self._collections = await self._get_json_response(_ENDPOINT_COLLECTIONS)
 
         return self._collections
 
     async def current_experience(self, use_cache=True) -> JsonDict:
         if self._current_experience is None or not use_cache:
-            self._current_experience = await self._get_json_response("/current")
+            self._current_experience = await self._get_json_response(
+                _ENDPOINT_CURRENT_EXPERIENCE
+            )
 
         return self._current_experience
 
     async def update_display_settings(self, settings: protocol.DisplaySettings) -> bool:
         async with self._aiohttp_session.put(
-            self._url_with_endpoint("/current"), json=settings
+            self._url_with_endpoint(_ENDPOINT_CURRENT_EXPERIENCE), json=settings
         ) as response:
             return response.ok
