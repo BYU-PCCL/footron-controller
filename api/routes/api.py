ad32894092e1e0cbced5e913406fb19daca30168
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-17 18:59:56 -0600

    Add PUT and PATCH methods for current experience

--- api/routes/api.py
+++ api/routes/api.py
@@ -1,4 +1,7 @@
+from typing import Union, Optional
+
 from fastapi import APIRouter
+from pydantic import BaseModel, validator
 
 from ..data import controller_api
 
@@ -6,6 +9,20 @@ router = APIRouter(
     prefix="/api",
     tags=["api"],
 )
+
+
+# TODO: Add authentication middleware + authentication state in data module
+
+# TODO: Consider moving these models somewhere (possibly to use Pydantic for models more broadly)
+class CurrentExperienceChange(BaseModel):
+    id: str
+
+
+class CurrentExperienceUpdate(BaseModel):
+    end_time: Optional[int] = None
+    lock: Optional[Union[bool, int]] = None
+    # TODO: Add a validator that at least one of these values is not none:
+    #  https://github.com/samuelcolvin/pydantic/issues/506#issuecomment-522255484 might be helpful
 
 
 @router.get("/experiences")
@@ -21,3 +38,13 @@ async def collections():
 @router.get("/current")
 async def current_experience():
     return await controller_api.current_experience(use_cache=False)
+
+
+@router.put("/current")
+async def set_current_experience(change: CurrentExperienceChange):
+    return await controller_api.set_current_experience(id=change.id)
+
+
+@router.patch("/current")
+async def update_current_experience(update: CurrentExperienceUpdate):
+    return await controller_api.set_current_experience(**update.dict())
