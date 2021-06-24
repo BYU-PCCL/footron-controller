92d06ff9b876c5255d1c2bbde19599a3441e3544
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-24 15:04:59 -0600

    Remove references to placard socket in timer for now @chris-luangrath

--- timer/timer.py
+++ timer/timer.py
@@ -1,17 +1,8 @@
 import copy
 import datetime
 import random
-import os
 import requests
-import requests_unixsocket
 import time
-import urllib.parse
-
-_PLACARD_SOCKETS_PATH = os.path.join(os.environ["XDG_RUNTIME_DIR"], "placard", "socket")
-_domain_sockets_session = requests_unixsocket.Session()
-_placard_escaped_url = (
-            f"http+unix://{urllib.parse.quote_plus(_PLACARD_SOCKETS_PATH)}"
-        )
 
 
 # Also gotta see what apps are actually able to be used (cameras online?)
@@ -43,20 +34,10 @@ cont = True
 def should_advance(start_time, app):
         current_app = requests.get("http://127.0.0.1:5000/current").json()
         if 'end_time' in current_app:
-            _domain_sockets_session.post(
-                f"{_placard_escaped_url}/end-time",
-                json={'end_time': current_app['end_time']},
-            )
-
             if datetime.datetime.now() < datetime.datetime.fromtimestamp(current_app['end_time']):
                 return False
         else:
-            end_time = start_time + (app['lifetime'])
             current_date = datetime.datetime.now().timestamp()
-            _domain_sockets_session.post(
-                f"{_placard_escaped_url}/end-time",
-                json={'end_time': end_time},
-            )
             if ((current_date - start_time) < app['lifetime']):
                 return False
 
