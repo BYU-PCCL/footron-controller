d33e39cb6f241d7793ba220bea80d2b0d6718e97
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-29 14:00:49 -0600

    Add CORS headers support

--- api/app.py
+++ api/app.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
+from .constants import BASE_URL
 from .routes import api, messaging
 
 app = FastAPI()
 
 app.include_router(api.router)
 app.include_router(messaging.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    # Note that we need to make sure BASE_URL starts with the protocol (http(s)://)
+    BASE_URL,
+]
+
+# TODO: See if there's any hardening we can do here
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", response_class=HTMLResponse)
