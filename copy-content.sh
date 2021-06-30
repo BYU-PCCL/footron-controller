da1b051f75f68e6dcd3cc5ed668442ad139ef81c
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-06-30 14:42:44 -0600

    Remove scripts for /opt/ copies

--- copy-content.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./make-cstv-dir.sh
-# cp -r ./content/* /opt/cstv
-sudo rsync -avu --delete content/ /opt/cstv/
--- make-cstv-dir.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p /opt/cstv/apps
-chmod -R 755 /opt/cstv
