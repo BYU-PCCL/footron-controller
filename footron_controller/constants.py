19565b83d3e4a7b92bf1b6aeb82c2580186f1bff
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-07-22 15:14:48 -0600

    Update constants

--- footron_controller/constants.py
+++ footron_controller/constants.py
@@ -2,10 +2,17 @@ import os
 from pathlib import Path
 from xdg import xdg_config_home
 
-# Once we automate app packaging and deployment, we should use XDG_DATA_HOME instead
-BASE_PATH = (
-    Path(os.environ["FT_BASE_PATH"])
-    if "FT_BASE_PATH" in os.environ
+BASE_DATA_PATH = (
+    Path(os.environ["FT_DATA_PATH"])
+    if "FT_DATA_PATH" in os.environ
     else Path(xdg_config_home(), "footron")
 )
-EXPERIENCES_PATH = Path(BASE_PATH, "experiences")
+
+# Once we automate app packaging and deployment, we should use XDG_DATA_HOME instead
+BASE_CONFIG_PATH = (
+    Path(os.environ["FT_CONFIG_PATH"])
+    if "FT_CONFIG_PATH" in os.environ
+    else Path(xdg_config_home(), "footron")
+)
+
+EXPERIENCES_PATH = Path(BASE_CONFIG_PATH, "experiences")
