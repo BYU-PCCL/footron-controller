77b32bef7fadb45bb19e9ca0bb9f81c270251d7b
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-08-25 16:45:27 -0600

    Try ZMQ pair pattern

--- footron_controller/data/wm.py
+++ footron_controller/data/wm.py
@@ -4,10 +4,8 @@ import zmq
 class WmApi:
     def __init__(self):
         self._context = zmq.Context()
-        self._socket = self._context.socket(zmq.REQ)
+        self._socket = self._context.socket(zmq.PAIR)
         self._socket.connect("tcp://localhost:5557")
 
     def set_fullscreen(self, fullscreen: bool):
         self._socket.send_json({"fullscreen": fullscreen})
-        # TODO: Do something with errors
-        _ = self._socket.recv_json()
