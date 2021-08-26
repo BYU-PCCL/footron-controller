d8e39d3e06c7554f0ecdf3ace8b0d8773054db29
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-08-26 16:09:49 -0600

    Try using Popen poll

--- footron_controller/util.py
+++ footron_controller/util.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 async def mercilessly_kill_process(process: subprocess.Popen):
     while True:
         process.terminate()
-        if process.returncode is not None:
+        if process.poll() is not None:
             break
         logger.warning(
             f"Managed process with pid {process.pid} didn't die, trying again in 1s..."
