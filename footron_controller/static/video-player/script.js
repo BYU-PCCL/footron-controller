f5365714903af43c7641202fa671cf818af49e69
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-09-18 00:47:31 -0600

    Add support for simple video scrubber protocol

--- footron_controller/static/video-player/script.js
+++ footron_controller/static/video-player/script.js
@@ -1,5 +1,8 @@
 /** @type {HTMLVideoElement} */
 const videoElement = document.querySelector("#video")
+const stateIconContainer = document.querySelector("#state-icon-container")
+
+const SEEK_EPSILON_S = 5;
 
 document.addEventListener("DOMContentLoaded", () => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -8,6 +11,49 @@ document.addEventListener("DOMContentLoaded", () => {
     // noinspection JSIgnoredPromiseFromCall
     videoElement.poster = urlParams.get('posterUrl')
     videoElement.play()
+
+    const client = new FootronMessaging.Messaging();
+
+    const play = () => {
+        videoElement.play()
+        stateIconContainer.style.opacity = "";
+    }
+
+    const pause = () => {
+        videoElement.pause()
+        stateIconContainer.style.opacity = "1";
+    }
+
+    const messageHandler = (message) => {
+        if (message.type === "toggle") {
+            let state;
+            if (videoElement.paused) {
+                play()
+                state = "playing";
+            } else {
+                pause()
+                state = "paused";
+            }
+
+            client.sendMessage({type: "state", state});
+        } else if (message.type === "scrub") {
+            const newTime = videoElement.duration * message.progress;
+            if (Math.abs(videoElement.currentTime - newTime) > SEEK_EPSILON_S) {
+                videoElement.currentTime = newTime;
+            }
+        } else if (message.type === "jump") {
+            videoElement.currentTime += message.delta;
+        }
+    }
+
+    const connectionHandler = (connection) => {
+        connection.addCloseListener(play)
+    }
+
+    client.mount();
+    client.addMessageListener(messageHandler);
+
+    client.addConnectionListener(connectionHandler)
 
     setInterval(() => {
         if (!videoElement.duration || !videoElement.currentTime) {
@@ -23,5 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
             },
             body: JSON.stringify({"id": videoId, "end_time": Math.floor(endTime * 1000)}),
         })
+
+        client.sendMessage({"type": "progress", "progress": videoElement.currentTime / videoElement.duration, "duration": videoElement.duration})
     }, 500)
 });
