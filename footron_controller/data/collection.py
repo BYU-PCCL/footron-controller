01d239257234b165c90d095a4ef9c813972ba3d6
chris-luangrath <thechrisluangrath@gmail.com>
2021-09-27 16:30:11 -0600

    doesn 't use individual loading now

--- footron_controller/data/collection.py
+++ footron_controller/data/collection.py
@@ -9,15 +9,3 @@ from ..constants import BASE_DATA_PATH
 class Collection:
     id: str
     experiences: List[str]
-
-
-def load_collections_from_fs(path=BASE_DATA_PATH) -> Dict[str, Collection]:
-    collections_file_path = path.joinpath("collections.json")
-
-    if not collections_file_path.exists():
-        return {}
-
-    with open(collections_file_path) as collections_file:
-        collection_data = json.load(collections_file)
-
-    return {id: Collection(id=id, **value) for id, value in collection_data.items()}
--- footron_controller/data/folder.py
+++ footron_controller/data/folder.py
@@ -12,15 +12,3 @@ class Folder:
     tags: List[str]
     title: str
     description: Optional[str]
-
-
-# def load_folders_from_fs(path=BASE_DATA_PATH) -> Dict[str, Folder]:
-#     folders_file_path = path.joinpath("folders.json")
-
-#     if not folders_file_path.exists():
-#         return {}
-
-#     with open(folders_file_path) as folders_file:
-#         folder_data = json.load(folders_file)
-
-#     return {id: Folder(id=id, **value) for id, value in folder_data.items()}
