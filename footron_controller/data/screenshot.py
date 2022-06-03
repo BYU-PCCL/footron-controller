a784612b71eb876c96214cdf0dfaed29af05482f
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-06-03 12:08:13 -0600

    Add support for capturing only the experience viewport

--- footron_controller/data/screenshot.py
+++ footron_controller/data/screenshot.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
-import io
-from PIL import ImageGrab
 from typing import TYPE_CHECKING, Optional
-
-if TYPE_CHECKING:
-    from PIL import Image
+import Xlib
+import Xlib.display
+from Xlib.xobject.drawable import Window
+from PIL import Image
 
 
 SCREENSHOT_MIME_TYPES = {
@@ -14,32 +13,34 @@ SCREENSHOT_MIME_TYPES = {
 }
 
 
-def take_screenshot() -> Image:
-    return ImageGrab.grab()
+class ScreenshotCapture:
+    def __init__(self):
+        self._display = Xlib.display.Display()
+        self._root: Window = self._display.screen().root
+        self._net_wm_name_atom = self._display.intern_atom("_NET_WM_NAME")
 
+    def _window_by_name(self, name: str) -> Optional[Window]:
+        children = self._root.query_tree().children
+        for child in children:
+            # Note here that we only search through windows which implement the newer
+            # _NET_WM_NAME atom, because that's what the specific window we're looking
+            # for uses
+            wm_name = child.get_full_property(self._net_wm_name_atom, 0)
+            if not wm_name or not wm_name.value:
+                continue
+            decoded_name = wm_name.value.decode("utf8")
+            if decoded_name == name:
+                return child
 
-async def create_screenshot_bytes_generator(
-    width: Optional[int] = None,
-    height: Optional[int] = None,
-    quality: int = 95,
-    format: str = "jpeg",
-):
-    image = take_screenshot()
-    image_width, image_height = image.size
-    image_bytes = io.BytesIO()
+    @staticmethod
+    def _capture_window(window: Window) -> Image:
+        geometry = window.get_geometry()
+        width, height = geometry.width, geometry.height
+        raw_image = window.get_image(0, 0, width, height, Xlib.X.ZPixmap, 0xFFFFFFFF)
+        return Image.frombytes("RGB", (width, height), raw_image.data, "raw", "BGRX")
 
-    width = width if width is not None else image_width
-    height = height if height is not None else image_height
+    def capture_root(self):
+        return self._capture_window(self._root)
 
-    ratio = max(min(width / image_width, height / image_height, 1), 0)
-
-    if ratio != 1 and ratio != 0:
-        image = image.resize((int(ratio * image_width), int(ratio * image_height)))
-
-    format_params = {}
-
-    if format == "jpeg":
-        format_params = {"quality": quality}
-
-    image.save(image_bytes, format=format, **format_params)
-    yield image_bytes.getvalue()
+    def capture_viewport(self):
+        return self._capture_window(self._window_by_name("FOOTRON_EXPERIENCE_VIEWPORT"))
